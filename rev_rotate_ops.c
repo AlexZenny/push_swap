f74ff8bffd6a06c60ce8a7c5faf1e5b4780ac66d
tia_malyshi <tetiana.malyshivska@gmail.com>
2026-08-19 22:33:13 +0200

    Adding rev_rotate

---
+++ rev_rotate_ops.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rev_rotate_ops.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: buhankalinux <buhankalinux@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/19 22:12:30 by buhankalinu       #+#    #+#             */
+/*   Updated: 2026/08/19 22:32:32 by buhankalinu      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_rev_rotate(t_list **stack)
+{
+	t_list	*temp;
+
+	if (stack == NULL || *stack == NULL)
+		return ;
+	*stack = (*stack)->prev;
+}
+void	ft_rev_rotate_both(t_list **stack_a, t_list **stack_b)
+{
+	if (stack_a == NULL || *stack_a == NULL || 
+		stack_b == NULL || *stack_b == NULL)
+		return ;
+	ft_rev_rotate(stack_a);
+	ft_rev_rotate(stack_b);
+}
