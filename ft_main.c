dd94e2bcefcbf243f23175a184b44b00bd76846f
azieniuk <azieniuk@student.42warsaw.pl>
2026-08-30 21:44:28 +0000

    Just another work in progress update.

--- ft_main.c
+++ ft_main.c
@@ -6,74 +6,75 @@
 /*   By: buhankalinux <buhankalinux@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/15 21:43:06 by buhankalinu       #+#    #+#             */
-/*   Updated: 2026/08/28 21:46:36 by buhankalinu      ###   ########.fr       */
+/*   Updated: 2026/08/30 13:58:59 by azieniuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include <stdio.h>
 
-void run_sort(t_list **stack_a, t_list **stack_b, t_options *options)
+void	run_sort(t_list **stack_a, t_list **stack_b, t_options *options)
 {
-    float disorder;
+	float	disorder;
 
-    disorder = calculate_disorder(stack_a);
-    if (options.mode == SIMPLE)
-        simple_sort(&stack_a, &stack_b);
-    if else (options.mode == MEDIUM)
-        medium_sort(&stack_a, &stack_b, stack_len(*stack_a));
-    if else (options.mode == COMPLEX)
-        complex_sort(&stack_a, &stack_b);
-    else  
-        adaptive_sort(&stack_a, &stack_b, disorder);
+	disorder = calculate_disorder(stack_a);
+	if (options.mode == SIMPLE)
+		simple_sort(&stack_a, &stack_b);
+	if else (options.mode == MEDIUM)
+		medium_sort(&stack_a, &stack_b, stack_len(*stack_a));
+	if else (options.mode == COMPLEX)
+		complex_sort(&stack_a, &stack_b);
+	else
+		adaptive_sort(&stack_a, &stack_b, disorder);
 }
-int ft_mode_status(t_options *options, t_mode mode)
+
+int	ft_mode_status(t_options *options, t_mode mode)
 {
-    if (options->mode_status == 1)
-        return(1);
-    options->mode = mode;
-    options->mode_status = 1; 
-    return(0);
+	if (options->mode_status == 1)
+		return (1);
+	options->mode = mode;
+	options->mode_status = 1;
+	return (0);
 }
-int ft_flags(const char *cursor, t_options *options)
-{   
-    if ((ft_strncmp(cursor, "--simple", 9) == 0))
-        return(ft_mode_status(options, SIMPLE));
-    else if ((ft_strncmp(cursor, "--medium", 9) == 0))
-        options->mode = MEDIUM;
-    else if ((ft_strncmp(cursor, "--complex", 10) == 0) && (options->mode_status == 0))
-        options->mode = COMPLEX;
-    else if ((ft_strncmp(cursor, "--adaptive", 11) == 0) && (options->mode_status == 0))
-        options->mode = ADAPTIVE;
-    else if (ft_strncmp(cursor, "--bench", 8) == 0)
-        options->bench = true;
-    else
-        return(1);
-    return(0);
-}
-int input_parser(int argc, char *argv[], t_options *options)
+
+int	ft_flags(const char *cursor, t_options *options)
 {
-    char *cursor;
-    int i;
-    
-    i = 1;
-    while ( i < argc)
-    {
-        cursor = argv[i];
-        if (cursor[0] == '-' && cursor[1] == '-' )
-        {   
-            if(ft_flags(cursor, options) == 1)
-                return(1);
-        }
-        else if (ft_is_number(cursor) == 1)
-            return(1);
-
-        i++;
-    } 
-    return(0);
+	if ((ft_strncmp(cursor, "--simple", 9) == 0))
+		return (ft_mode_status(options, SIMPLE));
+	else if ((ft_strncmp(cursor, "--medium", 9) == 0))
+		options->mode = MEDIUM;
+	else if ((ft_strncmp(cursor, "--complex", 10) == 0) && (options->mode_status == 0))
+		options->mode = COMPLEX;
+	else if ((ft_strncmp(cursor, "--adaptive", 11) == 0) && (options->mode_status == 0))
+		options->mode = ADAPTIVE;
+	else if (ft_strncmp(cursor, "--bench", 8) == 0)
+		options->bench = true;
+	else
+		return (1);
+	return (0);
 }
 
-void	set_data(t_data data);
+int	input_parser(int argc, char *argv[], t_options *options)
+{
+	char	*cursor;
+	int		i;
+
+	i = 1;
+	while (i < argc)
+	{
+		cursor = argv[i];
+		if (cursor[0] == '-' && cursor[1] == '-' )
+		{
+			if (ft_flags(cursor, options) == 1)
+				return (1);
+		}
+		else if (ft_is_number(cursor) == 1)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	set_data(t_data data)
 {
 	data.sort_mode = ADAPTIVE;
 	data.stack_a = NULL;
@@ -81,33 +82,40 @@ void	set_data(t_data data);
 	data.bench_mode = false;
 }
 
-int main(int argc, char* argv[])
+int	main(int argc, int **argv)
 {
-	t_data		data;
-    t_options   options;
-    int           status;
-    t_list       *stack_a;
-    t_list       *stack_b;
-    
-    options.mode = ADAPTIVE;
-    options.mode_status = 0;
-    options.bench = false;
-    stack_a = NULL;
-    stack_b = NULL;
-    
-    status = input_parser(argc, argv, &options);
-    if (status == 1)
-        printf("Error\n");
-    else if (status == 0)
-    {
-        status = ft_create_stack(argc, argv, &stack_a);
-        if (status == 1)
-        {
-            printf("Error\n");
-            ft_deallocate(&stack_a);
-            return(1);
-        }
-    }
-    simple_sorting(&stack_a, &stack_b);
-    ft_print_stacks(2, &stack_a, &stack_b);
+	t_data	data;
+	bool	status;
+
+	data = malloc(sizeof(t_data));
 }
+
+// int main(int argc, char* argv[])
+// {
+//     t_options   options;
+//     int           status;
+//     t_list       *stack_a;
+//     t_list       *stack_b;
+//     
+//     options.mode = ADAPTIVE;
+//     options.mode_status = 0;
+//     options.bench = false;
+//     stack_a = NULL;
+//     stack_b = NULL;
+//     
+//     status = input_parser(argc, argv, &options);
+//     if (status == 1)
+//         printf("Error\n");
+//     else if (status == 0)
+//     {
+//         status = ft_create_stack(argc, argv, &stack_a);
+//         if (status == 1)
+//         {
+//             printf("Error\n");
+//             ft_deallocate(&stack_a);
+//             return(1);
+//         }
+//     }
+//     simple_sorting(&stack_a, &stack_b);
+//     ft_print_stacks(2, &stack_a, &stack_b);
+// }
