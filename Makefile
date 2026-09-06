fb51207e0f4e1e6856346b1a0b79a4861d52f9e7
tmalyshi <tmalyshi@student.42warsaw.pl>
2026-09-06 22:19:41 +0200

    tiny sort working

--- Makefile
+++ Makefile
@@ -20,7 +20,6 @@ CFILES = push_swap.c\
 	init_structs.c\
 	split.c\
 	benchmark.c\
-	ft_debug_stacks.c
 
 OBJ_DIR = obj
 
--- algorithms/tiny_sorting.c
+++ algorithms/tiny_sorting.c
@@ -6,7 +6,7 @@
 /*   By: tmalyshi <tmalyshi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/31 20:47:56 by azieniuk          #+#    #+#             */
-/*   Updated: 2026/09/06 21:09:41 by tmalyshi         ###   ########.fr       */
+/*   Updated: 2026/09/06 22:19:22 by tmalyshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,21 @@ static void	three_sort(t_data *data)
 	int	b;
 	int	c;
 
-	a = data->stack_a->rank;
-	b = data->stack_a->next->rank;
-	c = data->stack_a->prev->rank;
-	if (a > b && b < c && a < c)
-		sa(data);
-	else if (a > b && b > c)
-		sa(data);
-	else if (a > b && a > c)
+	a = data->stack_a->value;
+	b = data->stack_a->next->value;
+	c = data->stack_a->prev->value;
+	if (a > b && b < c && c < a)  
 		ra(data);
-	else if (a < b && a > c)
+	else if (a > b && b > c && c < a)
+	{
+		sa(data);
 		rra(data);
-	else if (a < b && b > c)
+	} 
+	else if (a < b && b > c && a > c) 
+		rra(data);
+	else if (a > b && c > b && a < c) 
+		sa(data);
+	else if (a < b && a < c && b > c) 
 	{
 		rra(data);
 		sa(data);
@@ -77,7 +80,6 @@ static void	five_sort(t_data *data)
 				rra(data);
 		}
 		pb(data);
-		ft_print_stacks(1, &data->stack_a, &data->stack_b);
 		i++;
 	}
 	three_sort(data);
